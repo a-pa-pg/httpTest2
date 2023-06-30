545a46a61e492d6baa9e25c11fc0aee6106bbca7
yama <yama@host.docker.internal>
2023-07-01 03:08:09 +0900

    修正

--- httpTest/src/main/java/com/example/demo/Get.java
+++ httpTest/src/main/java/com/example/demo/Get.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/controller")
+@RequestMapping("/controller")
 public class Get {
-//	@GetMapping("/greeting")
-//	public String getgreeting(@RequestParam("param") String param) {
-//		return "test" + param;
-//	}
+
 	@GetMapping("/test")
 	public String index() {
 		return "hello";
